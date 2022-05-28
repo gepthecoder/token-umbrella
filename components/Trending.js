fc5a4e9dfcfa88b7b3507a073b79bd32ea6da9fd
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-28 16:31:38 +0200

    $0015: [Trending] [TrendingCard]: create trending card component and render it

--- components/Trending.js
+++ components/Trending.js
@@ -10,7 +10,9 @@ import recent from "../assets/recent.png"
 // HIGHLIGHT BUTTON
 import ReactSwitch from "react-switch"
 
+// COMPONENTS
 import Rate from "./cmc-table/Rate"
+import TrendingCard from "./TrendingCard"
 
 const styles = {
     trendingWrapper: `mx-auto max-w-screen-2xl`,
@@ -21,26 +23,56 @@ const styles = {
 const Trending = () => {
     const [checked, setChecked] = useState(false)
 
+    const trendingData = [
+        {
+            number: 1,
+            symbol: "BTC",
+            name: "Bitcoin",
+            icon: btc,
+            isIncrement: true,
+            rate: "2.34%"
+        }, {
+            number: 2,
+            symbol: "USDT",
+            name: "Tether",
+            icon: usdt,
+            isIncrement: false,
+            rate: "9.32%"
+        }, {
+            number: 1,
+            symbol: "BTC",
+            name: "Bitcoin",
+            icon: btc,
+            isIncrement: true,
+            rate: "2.34%"
+        },
+    ]
+
     return (
         <div className="text-white">
-        <div className={styles.trendingWrapper}>
-            <div className="flex justify-between">
-                <h1 className={styles.h1}>All Cryptocurrencies Under One Umbrella</h1>
+            <div className={styles.trendingWrapper}>
+                <div className="flex justify-between">
+                    <h1 className={styles.h1}>All Cryptocurrencies Under One Umbrella</h1>
 
+                    <div className="flex">
+                        <p className="text-gray-400 ">Highlights &nbsp;</p>
+                        <ReactSwitch checked={checked} onChange={() => { setChecked(!checked) }} />
+                    </div>
+                </div>
+                <br />
                 <div className="flex">
-                    <p className="text-gray-400 ">Highlights &nbsp;</p>
-                    <ReactSwitch checked={checked} onChange={() => { setChecked(!checked) }} />
+                    <p>The global crypto market cap is $1.74T, a &nbsp; </p>
+                    <span> <Rate isIncrement={true} rate='0.53%' /> </span>
+                    <p> &nbsp; decrease over the last day. <span className="underline">Read More</span> </p>
+                </div>
+                <br />
+                <div className={styles.flexCenter}>
+                    <TrendingCard title='Trending' icon={fire} trendingData={trendingData} />
+                    <TrendingCard title='Biggest Gainers' icon={gainers} trendingData={trendingData} />
+                    <TrendingCard title='Recently Added' icon={recent} trendingData={trendingData} />
                 </div>
             </div>
-            <br />
-            <div className="flex">
-                <p>The global crypto market cap is $1.74T, a &nbsp; </p>
-                <span> <Rate isIncrement={true} rate='0.53%' /> </span>
-                <p> &nbsp; decrease over the last day. <span className="underline">Read More</span> </p>
-            </div>
-            <br />
         </div>
-    </div>
     )
 }
 
---
+++ components/TrendingCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TrendingCard = () => {
+  return (
+    <div>TrendingCard</div>
+  )
+}
+
+export default TrendingCard
