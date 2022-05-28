afc29597e61a7f7bf3cbe7f7840886d5f771e809
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-28 15:54:04 +0200

    $0012: [Trending]: add style and react switch highlights checkmark

--- components/Trending.js
+++ components/Trending.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState } from "react"
+
+// ICONS
+import fire from "../assets/fire.png"
+import btc from "../assets/btc.png"
+import usdt from "../assets/usdt.png"
+import gainers from "../assets/gainers.png"
+import recent from "../assets/recent.png"
+
+// HIGHLIGHT BUTTON
+import ReactSwitch from "react-switch"
+
+const styles = {
+    trendingWrapper: `mx-auto max-w-screen-2xl`,
+    h1: `text-3xl text-white`,
+    flexCenter: `flex items-center,`
+}
 
 const Trending = () => {
-  return (
-    <div>Trending</div>
-  )
+    const [checked, setChecked] = useState(false)
+
+    return (
+        <div className="text-white">
+        <div className={styles.trendingWrapper}>
+            <div className="flex justify-between">
+                <h1 className={styles.h1}>All Cryptocurrency Under One Umbrella</h1>
+
+                <div className="flex">
+                    <p className="text-gray-400 ">Highlights &nbsp;</p>
+                    <ReactSwitch checked={checked} onChange={() => { setChecked(!checked) }} />
+                </div>
+            </div>
+            <br />
+        </div>
+    </div>
+    )
 }
 
 export default Trending
