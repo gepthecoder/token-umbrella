f9c7b2c28c702bce88b53e29cdcfab575b8a4a03
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-28 15:40:14 +0200

    $0011: [Trending]: create trending component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
 import Header from '../components/Header'
+import Trending from '../components/Trending'
 
 const Home: NextPage = () => {
   return (
     <div className='min-h-screen'>
       <Header />
       <div className='mt-10' />
-      {/* <Trending /> */}
+      <Trending />
       <div className='mt-20' />
       {/* <CMCtable /> */}
     </div>
