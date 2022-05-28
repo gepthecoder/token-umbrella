6f80a2d689028fad7fc3f29c3dc0f2905e69effe
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-28 13:46:09 +0200

    $0009: [Header]: badge style

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ import Search from '../assets/svg/search'
 const styles = {
     header: `bg-[#17171A] text-white h-20 flex gap-[100px] w-full p-[20px]`,
     navLink: `text-white flex mx-[10px]`,
-    badge: `rounded-full bg-blue-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
+    badge: `rounded-full bg-cyan-600 h-1 w-1 absolute bottom-5 right-0 top-1 ring-4`,
     navItem: `relative mr-1 cursor-pointer hover:opacity-60`,
     nav: `flex justify-center items-center gap-[20px]`,
     headerWrapper: `flex justify-center h-full max-w-screen-xl mx-auto px-4`,
