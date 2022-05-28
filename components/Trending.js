ac2210a83c9f1b675c7c149f4ee977507363ca9d
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-28 16:26:14 +0200

    $0013: [Trending]: add rate component, title and crypto market cap info

--- components/Trending.js
+++ components/Trending.js
@@ -10,6 +10,8 @@ import recent from "../assets/recent.png"
 // HIGHLIGHT BUTTON
 import ReactSwitch from "react-switch"
 
+import Rate from "./cmc-table/Rate"
+
 const styles = {
     trendingWrapper: `mx-auto max-w-screen-2xl`,
     h1: `text-3xl text-white`,
@@ -23,12 +25,18 @@ const Trending = () => {
         <div className="text-white">
         <div className={styles.trendingWrapper}>
             <div className="flex justify-between">
-                <h1 className={styles.h1}>All Cryptocurrency Under One Umbrella</h1>
+                <h1 className={styles.h1}>All Cryptocurrencies Under One Umbrella</h1>
 
                 <div className="flex">
                     <p className="text-gray-400 ">Highlights &nbsp;</p>
                     <ReactSwitch checked={checked} onChange={() => { setChecked(!checked) }} />
                 </div>
+            </div>
+            <br />
+            <div className="flex">
+                <p>The global crypto market cap is $1.74T, a &nbsp; </p>
+                <span> <Rate isIncrement={true} rate='0.53%' /> </span>
+                <p> &nbsp; decrease over the last day. <span className="underline">Read More</span> </p>
             </div>
             <br />
         </div>
