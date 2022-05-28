e3e6ad4d270b08e94e2699e6a6b51f5f1ffd87e9
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-28 21:48:01 +0200

    $0018: [TrendingCard] [MoreButton]: style button

--- components/MoreButton.js
+++ components/MoreButton.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import RightArrow from "../assets/svg/rightArrow"
+
+const styles = {
+    button: `text-[#6188FF] flex items-center cursor-pointer whitespace-nowrap justify-between`
+}
 
 const MoreButton = () => {
-  return (
-    <div>MoreButton</div>
-  )
+    return <div className={styles.button}>More <RightArrow /></div>
 }
 
 export default MoreButton
