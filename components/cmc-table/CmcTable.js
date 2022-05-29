13d61b93f545031e1efe050cf679b3508a9bce0e
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-29 11:42:20 +0200

    $0026: [CmcTable]: set coin data for top10

--- components/cmc-table/CmcTable.js
+++ components/cmc-table/CmcTable.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import { useContext, useEffect, useState, useCallback } from 'react'
+
+// ASSETS
+import btc from '../../assets/btc.png'
+
+// CONTEXT
+import { TokenUmbrellaContext } from '../../context/context'
 
 const CmcTable = () => {
+    let { getTopTenCoins } = useContext(TokenUmbrellaContext)
+
+    let [coinData, setCoinData] = useState(null)
+
+    useEffect(() => {
+        setData()
+      }, [])
+
+    const setData = useCallback(async () => {
+        try {
+            let apiResponse = await getTopTenCoins()
+            let filteredResponse = []
+        
+            for (let i = 0; i < apiResponse.length; i++) {
+                const element = apiResponse[i]
+                if (element.cmc_rank <= 10) filteredResponse.push(element)
+            }
+    
+            setCoinData(filteredResponse)
+        } catch (e) { console.log(e.message) }
+        console.log(coinData, '🧿')
+    }, [getTopTenCoins])
+
   return (
     <div>CmcTable</div>
   )
