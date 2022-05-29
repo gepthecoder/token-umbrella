ccef6b07600108aaead4a703c8f25d5a33d0bd42
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-29 11:17:15 +0200

    $0023: [Context]: make getTopTenCoins accessable globaly

--- context/context.js
+++ context/context.js
@@ -5,10 +5,20 @@ export const TokenUmbrellaContext = createContext()
 
 export const TokenUmbrellaProvider = ({ children }) => {
 
+    const getTopTenCoins = async () => {
+        try {
+          const res = await fetch('/api/getTopTen')
+          const data = await res.json()
+          return data.data.data
+        } catch (e) {
+          console.log(e.message)
+        }
+    }
+
     return(
         <TokenUmbrellaContext.TokenUmbrellaProvider
             value= {{
-                
+                getTopTenCoins
             }}
         >
             {children}
