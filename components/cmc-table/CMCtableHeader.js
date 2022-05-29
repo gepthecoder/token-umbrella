b3402ac3e9299ec0a1f422e444fc6b711a0f5bfd
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-29 11:49:56 +0200

    $0027: [CmcTable] [TableHeader]: create cmc table header component

---
+++ components/cmc-table/CMCtableHeader.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CMCtableHeader = () => {
+  return (
+    <div>CMCtableHeader</div>
+  )
+}
+
+export default CMCtableHeader
--- components/cmc-table/CmcTable.js
+++ components/cmc-table/CmcTable.js
@@ -6,6 +6,10 @@ import btc from '../../assets/btc.png'
 // CONTEXT
 import { TokenUmbrellaContext } from '../../context/context'
 
+// COMPONENTS
+import CMCtableHeader from './CMCtableHeader'
+
+
 const CmcTable = () => {
     let { getTopTenCoins } = useContext(TokenUmbrellaContext)
 
@@ -27,11 +31,41 @@ const CmcTable = () => {
     
             setCoinData(filteredResponse)
         } catch (e) { console.log(e.message) }
-        console.log(coinData, '🧿')
     }, [getTopTenCoins])
 
   return (
-    <div>CmcTable</div>
+    <div className='text-white font-bold'>
+        <div className='mx-auto max-w-screen-2xl'>
+            <table className='w-full'>
+                <CMCtableHeader />
+
+                {coinData && coinData ? (
+                    coinData.map((coin, index) => {
+                        // return (
+                        //     // <CMCtableRow
+                        //     //     key={index}
+                        //     //     starNum={coin.cmc_rank}
+                        //     //     coinName={coin.name}
+                        //     //     coinSymbol={coin.symbol}
+                        //     //     coinIcon={btc}
+                        //     //     showBuy={true}
+                        //     //     hRate={coin.quote.USD.percent_change_24h}
+                        //     //     dRate={coin.quote.USD.percent_change_7d}
+                        //     //     hRateIsIncrement={true}
+                        //     //     price={coin.quote.USD.price}
+                        //     //     marketCapValue={coin.quote.USD.market_cap}
+                        //     //     volumeCryptoValue={coin.quote.USD.volume_24h}
+                        //     //     volumeValue={coin.total_supply}
+                        //     //     circulatingSupply={coin.circulating_supply}
+                        //     // />
+                        // )
+                    })
+                ) : (
+                    <></>
+                )}
+            </table>
+        </div>
+    </div>
   )
 }
 
