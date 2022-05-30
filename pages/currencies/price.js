c7725b3811df5377ac6188f4c0d0db62579d5eb9
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-30 09:48:13 +0200

    $0043: [PriceDataPage]: get data through params and pass it to newly created coin details component

--- pages/currencies/price.js
+++ pages/currencies/price.js
@@ -1,8 +1,31 @@
-import Header from "../../components/header"
+// COMPONENTS
+import Header from "../../components/Header"
+import CoinDetails from "../../components/CoinDetails"
+
+//HOOKS
+import { useState, useEffect } from "react"
 
 const Price = () => {
+    const [coinName, setCoinName] = useState('')
+    const [coinSymbol, setCoinSymbol] = useState('')
+    const [price, setPrice] = useState('')
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+
+        setCoinName(urlParams.get('coin'));
+        setPrice(Number(urlParams.get('price')).toLocaleString(0));
+        setCoinSymbol(urlParams.get('symbol'));
+    }
+
     return <div>
         <Header />
+        <CoinDetails coinName={coinName} price={price} coinSymbol={coinSymbol} />
     </div>
 }
 
