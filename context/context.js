dd6917fe4eb7059edffc33367e6bc9f95a9fdf19
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-30 12:23:08 +0200

    $0050: [DynamicData] [Context]: create constants with static generated token data; dynamic context: auth - part1

--- context/context.js
+++ context/context.js
@@ -1,9 +1,39 @@
 import { createContext, useState, useEffect } from 'react'
+import { useMoralis, useMoralisQuery } from 'react-moralis'
 
+import {
+    dogeAbi,
+    daiAbi,
+    linkAbi,
+    usdcAbi,
+    dogeAddress,
+    linkAddress,
+    daiAddress,
+    usdcAddress,
+  } from '../lib/constants'
 
 export const TokenUmbrellaContext = createContext()
 
 export const TokenUmbrellaProvider = ({ children }) => {
+
+    const { isAuthenticated, user, Moralis } = useMoralis()
+
+    const [currentAccount, setCurrentAccount] = useState('')
+    // Modal for swaping tokens
+    const [openBuyCryptoModal, setOpenBuyCryptoModal] = useState(false)
+    // Tokens swap handling
+    const [fromToken, setFromToken] = useState('ETH')
+    const [toToken, setToToken] = useState('')
+    const [amount, setAmount] = useState('')
+
+    useEffect(() => {
+        if (isAuthenticated) {
+          const account = user.get('ethAddress')
+          setCurrentAccount(account)
+        }
+    }, [isAuthenticated])
+
+    // Todo: Get contract addresses
 
     const getTopTenCoins = async () => {
         try {
